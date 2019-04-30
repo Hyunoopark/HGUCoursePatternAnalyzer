cbb41eae717b3ec8d9cffbd864db751ebd073683
hyunwoo <hyunwoo@192.168.0.11>
2019-04-30 20:56:18 +0900

    first commit

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,10 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	private String courseName;
+	
+	public String getCourseName() {
+		return courseName;
+	}
+
+}
---
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -0,0 +1,9 @@
+package edu.handong.analysis.datamodel;
+
+public class Student {
+	private String student;
+	
+	public String getName() {
+		return student;
+	}
+}
